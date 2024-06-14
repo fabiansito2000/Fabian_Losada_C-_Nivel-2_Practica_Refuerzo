3abd3b66b858957dff9a9b4bbbeca80cefb5e7b1
Fabian Losada <fabianlosada1@hotmail.com>
2024-06-14 11:03:08 -0400

    Ejercicio_1

---
+++ #1_Contador de palabras en un texto/Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe La Palabra:");
+            string inputText = Console.ReadLine();
+
+            string[] words = inputText.Split(' ');
+
+            Dictionary<string, int> wordFrequencies = new Dictionary<string, int>();
+            
+            foreach (string word in words)
+            {
+                string cleanedWord = word.Trim().ToLower();
+                if (!string.IsNullOrEmpty(cleanedWord))
+                {
+                    if (wordFrequencies.ContainsKey(cleanedWord))
+                    {
+                        wordFrequencies[cleanedWord]++;
+                    }
+                    else
+                    {
+                        wordFrequencies[cleanedWord] = 1;
+                    }
+                }
+            }
+
+            Console.WriteLine("Palabra\tFrecuencia");
+            foreach (var entry in wordFrequencies)
+            {
+                Console.WriteLine($"{entry.Key}\t{entry.Value}");
+            }
+        }
+    }
+}
